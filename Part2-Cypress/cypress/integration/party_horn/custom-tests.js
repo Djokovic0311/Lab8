9c9f1e4fbd1b4e1f5eda162aa9332b4286deadf7
Djokovic0311 <1178328479@qq.com>
2021-02-24 15:59:23 +0800

    finish Part1

---
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -0,0 +1,17 @@
+describe('Party Horn Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
+  });
+
+  it('First Test', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('Slider changes when volume input changes', () => {
+    cy.get('#volume-number').clear().type('75');
+    cy.get('#volume-slider').then($el => {
+      expect($el).to.have.value(75);
+    });
+  });
+
+});
